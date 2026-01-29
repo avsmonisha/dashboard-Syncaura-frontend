0fe26ee884fd80573dfbfbc737129f3ed57c3f98
p-prince-n <pp2358296@gmail.com>
2026-01-29 18:03:10 +0530

    notification slice added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slices/themeSlice";
 import meetingReducer from "./slices/meetingSlice";
+import notificationReducer from "./slices/notificationSlice";
 
 export const store=configureStore({
     reducer:{
         theme: themeReducer,
-        meeting:meetingReducer
+        meeting:meetingReducer,
+        notification: notificationReducer
     }
 })
